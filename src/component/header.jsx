e6780e96b7afaa04c4f109b3f4377883b0a73b62
VuongDo78 <68509758+VuongDo78@users.noreply.github.com>
2021-08-29 13:18:46 +0700

    Update header.jsx

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -5,14 +5,10 @@ class Header extends Component {
     render() {
         return (
            <div>
-                <Nav bsStyle="tabs"  dark>
-                    <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
-                    <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
-                    <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
-                </Nav>
-                <Navbar bsStyle="tabs" dark color="primary">
+               
+                <Navbar dark color="primary">
                    
-                        <NavbarBrand href="/">Nhân Viên</NavbarBrand>
+                        <NavbarBrand href="/">Quản Lý Nhân Sự Ver 1.0</NavbarBrand>
                         <NavbarBrand href="/">Phòng Ban</NavbarBrand>
                         <NavbarBrand href="/">Bảng Lương</NavbarBrand>
                     
