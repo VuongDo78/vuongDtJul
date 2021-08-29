351eac229223e1e39b463ec8578f2835b1d5e9d1
VuongDo78 <68509758+VuongDo78@users.noreply.github.com>
2021-08-29 20:09:41 +0700

    Update MainComponent.jsx

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -13,27 +13,39 @@ class Main extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             staffs: STAFFS,
-            selectedStaff: null
+            selectedStaff: null,
+            classNameStaffList: "col-sm-6 col-md-4 mt-5",
         };
     }
 
     onStaffSelect(staffId) {
         this.setState({ selectedStaff: staffId });
     }
-
+    sixCol(classname) {
+        this.setState({ classNameStaffList: classname })
+    }
     render() {
         return (
             <div>
-                
-                <Header/>
 
+                <Header />
+                <button className="btn btn-dark mt-3 ml-3"
+                    onClick={() => { this.sixCol("col-sm-6 col-md-2 mt-5") }}
+                >Hiển thị 6 cột
+                </button>
+
+                <button className="btn btn-warning mt-3 ml-3"
+                    onClick={() => { this.sixCol("col-sm-6 col-md-4 mt-5") }}
+                >Hiển thị 3 cột</button>
+                
                 <div className="container">
-                    <StaffList staffs={this.state.staffs} />
-                    <StaffDetail staffs={this.state.staffs.filter((staff) => staff.id === this.state.selectedStaff)[1]} />
+                    <StaffList staffs={this.state.staffs} classNameStaffList={this.state.classNameStaffList} />
+                    <StaffDetail staffs={this.state.staffs.filter((staff) => staff.id === this.state.selectedStaff)[0]} />
                 </div>
-                <Footer/>
+                <Footer />
             </div>
         );
     }
