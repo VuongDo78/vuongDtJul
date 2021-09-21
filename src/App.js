dcdaeb04429627c5797bca6781fdce1b199c3d0d
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-09-21 22:50:19 +0700

    Lab3_2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import logo from './logo.svg';
-import React from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './components/shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render(){
+    return(
+    <div className = "App" >
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Confision</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
