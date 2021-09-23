1020759af24c94231b184a5fd4564b31eab644c4
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-09-23 18:56:38 +0700

    Lab_8_2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import dateFormat from 'dateformat';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
+import {
+
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -16,26 +21,31 @@ function RenderDish(props) {
         </Card>
     );
 }
-function RenderComments(props) {
+function RenderComments({ comments, addComment, dishId }) {
     return (
         <div className="col-12 col-md-6 m-1">
             {
-                props.comments.map(el =>
+                comments.map(el =>
                     <Card key={el.dishId}>
                         <CardText>{el.name}</CardText>
                         <CardText>{el.comment}</CardText>
                         <CardText>Đánh giá {el.rating} sao</CardText>
-                        <CardText>{dateFormat(props.comments.date)}</CardText>
+                        <CardText>{dateFormat(comments.date)}</CardText>
                         <CardText>{el.author}</CardText>
                     </Card>
                 )
             }
+        <CommentForm dishId={dishId} addComment={addComment} />   
+        
+
         </div>
     )
 }
-const DishDetail = (props) => {
-    console.log('props.comments', props.comments);
 
+
+
+
+const DishDetail = (props) => {
     if (props.dish != null) {
 
         const selectedDishes = props.dish;
@@ -59,8 +69,13 @@ const DishDetail = (props) => {
                         <RenderDish dish={selectedDishes} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
+                           
+                        
                     </div>
+                   
                 </div>
             </div>
         );
@@ -70,6 +85,68 @@ const DishDetail = (props) => {
         return null
 }
 
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, this.rating.value, 
+            this.author.value, this.comment.value);
+    }
+    render() {
+        return (
+            <div>
+                <Button onClick={this.toggleModal}>
+                                 <span className="btn btn-light"></span>Submit Comment</Button>
+                                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Label htmlFor="rating">Rating</Label>
+                            <Input type="select" id="rating" name="rating"
+                                innerRef={(input) => this.rating = input}>
+                                    <option value ="1" >1</option>
+                                    <option  value ="2" >2</option>
+                                    <option  value ="3" >3</option>
+                                    <option  value ="4" >4</option>
+                                    <option  value ="5" >5</option>
+                                    
+                                </Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="author">Họ Tên</Label>
+                            <Input type="text" id="author" name="author"
+                                innerRef={(input) => this.author = input} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="comment">Comment</Label>
+                            <Input type="text" id="comment" name="comment"
+                                innerRef={(input) => this.comment = input}/>
+                        </FormGroup>
+                        <Button type="submit" value="submit" color="primary">Submit</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
+            </div>
+            
+        )
+    }
+}
 
 export default DishDetail;
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import { addComment } from '../redux/ActionCreators';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from './shared/dishes';
@@ -14,7 +14,11 @@ import { LEADERS } from './shared/leaders';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
 
+});
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
@@ -55,8 +59,10 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}
+      />
       );
     };
 
@@ -79,4 +85,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
