9298a428edb3872a0f23c466e2f78ed7a53ba590
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-10-26 20:34:44 +0700

    Lab1_3

--- Lab1_2.js
+++ Lab1_2.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-    console.log(res);
+    res.setHeader(200, { 'Content-Type': 'text/html' });
+    res.write("Hello from my Node.js server!");
 });
 server.listen(3000);
--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 
 //fs.writeFileSync("notes.txt", "this file was created by Node.js");
 
-fs.appendFileSync("notes.txt", "My name Vuong");
+fs.appendFileSync("notes.txt", "My name Vuong 25yrs");
