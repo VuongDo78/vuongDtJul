fefe24ad7991fcc474e68d49044491ee43844224
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-10-26 21:17:02 +0700

    Lab1_4

--- Lab1_2.js
+++ Lab1_2.js
@@ -1,6 +1,21 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-    res.setHeader(200, { 'Content-Type': 'text/html' });
-    res.write("Hello from my Node.js server!");
+    res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    if (url === "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.write('<head><title>Assment</title></head>');
+        res.write('<body><h1>welcome and hello</h1></body>');
+        res.write('<form action = "/message" method = "POST"><input type="text" name="username"></input><button type = "submit">send</button></form></form>')
+        return res.end();
+    }
+    if (url === "/message") {
+        res.setHeader("Content-Type", "text/html");
+        res.write('<head><title>Assment</title></head>');
+        res.write("<body>Hello from my Node.js server!</body>");
+        return res.end();
+    }
+
+
 });
 server.listen(3000);
