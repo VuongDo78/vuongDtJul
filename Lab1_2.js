0c33173f35ee83d42eb3c1ce485d91259ad77ff7
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-10-28 09:31:28 +0700

    Lab1_5

--- Lab1_2.js
+++ Lab1_2.js
@@ -2,20 +2,21 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     const url = req.url;
+    const fs = require('fs');
     if (url === "/") {
         res.setHeader("Content-Type", "text/html");
         res.write('<head><title>Assment</title></head>');
         res.write('<body><h1>welcome and hello</h1></body>');
         res.write('<form action = "/message" method = "POST"><input type="text" name="username"></input><button type = "submit">send</button></form></form>')
-        return res.end();
+        res.end();
     }
     if (url === "/message") {
-        res.setHeader("Content-Type", "text/html");
-        res.write('<head><title>Assment</title></head>');
-        res.write("<body>Hello from my Node.js server!</body>");
-        return res.end();
+        fs.appendFileSync("message.txt", "DUMMY");
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
     }
 
-
 });
+
 server.listen(3000);
