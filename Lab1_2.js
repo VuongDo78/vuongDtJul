7e2ab7c6e5881a4602f36a0fbb782fad40a6ddcd
Wong Do <vuongdtfx02170@funix.edu.vn>
2021-10-28 10:30:58 +0700

    Lab1_7

--- Lab1_2.js
+++ Lab1_2.js
@@ -1,32 +1,5 @@
 const http = require("http");
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    const url = req.url;
-    const method = req.method;
-
-    const fs = require('fs');
-    if (url === "/") {
-        res.setHeader("Content-Type", "text/html");
-        res.write('<head><title>Assment</title></head>');
-        res.write('<body><h1>welcome and hello</h1></body>');
-        res.write('<form action = "/message" method = "POST"><input type="text" name="username"></input><button type = "submit">send</button></form></form>')
-        res.end();
-    }
-    if (url === "/message" && method === "POST") {
-        const body = [];
-
-        req.on("data", (chunk) => {
-            body.push(chunk);
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            fs.appendFileSync("message.txt", " " + parseBody + " ");
-        })
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end();
-    }
-
-});
+const router = require("./router");
+const server = http.createServer(router);
 
 server.listen(3000);
